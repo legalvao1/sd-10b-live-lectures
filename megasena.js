821167b896c6a23e2d467c4a80ddefa86887104c
Gustavo Caetano <gustavo.caetano@betrybe.com>
2021-02-25 17:45:04 -0300

    checador da mega-sena com 1 jogo

--- megasena.js
+++ megasena.js
@@ -9,4 +9,18 @@ let sixthNumber = Math.ceil(Math.random() * 60);
 
 megaSenaNumbers = [firstNumber, secondNumber, thirdNumber, fourthNumber, fifthNumber, sixthNumber];
 
-console.log(megaSenaNumbers);
+let turma10Play = [20, 40, 35, 9, 17, 52];
+
+let numberOfHits = 0;
+
+for (let indexMega = 0; indexMega < megaSenaNumbers.length; indexMega += 1) {
+  for (let indexPlay = 0; indexPlay < turma10Play.length; indexPlay += 1) {
+    if (megaSenaNumbers[indexMega] === turma10Play[indexPlay]) {
+      numberOfHits += 1;
+    }
+  }
+}
+
+console.log(`Mega-Sena: ${megaSenaNumbers}`);
+console.log(`Jogo da Turma 10: ${turma10Play}`);
+console.log("Acertos: " + numberOfHits);
