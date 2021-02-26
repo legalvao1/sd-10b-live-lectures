b0afd6f935bb3142f99b209010a74a08737ad9de
Gustavo Caetano <gustavo.caetano@betrybe.com>
2021-02-26 18:06:38 -0300

    adiciona todas as diagonais da rainha

--- queen-attack.js
+++ queen-attack.js
@@ -3,8 +3,8 @@ let posicaoRainhaLinha = 4;
 let posicaoRainhaColuna = 4;
 
 //posicao da peça alvo
-let posicaoPecaLinha = 7;
-let posicaoPecaColuna = 7;
+let posicaoPecaLinha = 8;
+let posicaoPecaColuna = 8;
 
 //identifica se o ataque foi bem sucedido ou não
 let ataqueBemSucedido = false;
@@ -27,6 +27,46 @@ for (let supDir = 1; supDir < 8; supDir += 1) {
   }
 }
 
+//Diagonal superior esquerda
+for (let supEsq = 1; supEsq < 8; supEsq += 1) {
+  let linhaRainha = posicaoRainhaLinha + supEsq;
+  let colunaRainha = posicaoRainhaColuna - supEsq;
 
+  if (linhaRainha > 8 || colunaRainha < 1) {
+    break;
+  }
+
+  if ((posicaoPecaLinha == linhaRainha) && (posicaoPecaColuna == colunaRainha)) {
+    ataqueBemSucedido = true;
+  }
+}
+
+//Diagonal inferior esquerda
+for (let infEsq = 1; infEsq < 8; infEsq += 1) {
+  let linhaRainha = posicaoRainhaLinha - infEsq;
+  let colunaRainha = posicaoRainhaColuna - infEsq;
+
+  if (linhaRainha < 1 || colunaRainha < 1) {
+    break;
+  }
+
+  if ((posicaoPecaLinha == linhaRainha) && (posicaoPecaColuna == colunaRainha)) {
+    ataqueBemSucedido = true;
+  }
+}
+
+//Diagonal inferior direita
+for (let infDir = 1; infDir < 8; infDir += 1) {
+  let linhaRainha = posicaoRainhaLinha - infDir;
+  let colunaRainha = posicaoRainhaColuna + infDir;
+
+  if (linhaRainha < 1 || colunaRainha > 8) {
+    break;
+  }
+
+  if ((posicaoPecaLinha == linhaRainha) && (posicaoPecaColuna == colunaRainha)) {
+    ataqueBemSucedido = true;
+  }
+}
 
 console.log(ataqueBemSucedido);
