ed3e99a708d188b9e5b06ffc0f634d29de344834
Gustavo Caetano <gustavo.caetano@betrybe.com>
2021-03-05 18:10:41 -0300

    Carrinhos completos e bonitos

--- script.js
+++ script.js
@@ -3,11 +3,30 @@ window.onload = function () {
   let car1 = document.querySelector('.car1');
   let car2 = document.querySelector('.car2');
 
+  car1.style.marginLeft = '0px';
+  car2.style.marginLeft = '0px';
+
   //1. qual o tipo do evento devo escutar?
   //2. o que eu faço quando esse evento acontecer?
   button.addEventListener('click', function () {
-    car1.style.marginLeft = Math.random() * 100 + 'px';
-    car2.style.marginLeft = Math.random() * 100 + 'px';
+    car1.style.marginLeft = parseInt(car1.style.marginLeft) + (Math.random() * 100) + 'px';
+    car2.style.marginLeft = parseInt(car2.style.marginLeft) + (Math.random() * 100) + 'px';
+
+    if (parseInt(car1.style.marginLeft) > window.innerWidth) {
+      alert('Carro VERMELHO ganhou!');
+      resetCars(car1, car2);
+    }
+
+    if (parseInt(car2.style.marginLeft) > window.innerWidth) {
+      alert('Carro VERDE ganhou!');
+      resetCars(car1, car2);
+    }
+
+
   });
 };
 
+function resetCars(car1, car2) {
+  car1.style.marginLeft = '0px';
+  car2.style.marginLeft = '0px';
+}
