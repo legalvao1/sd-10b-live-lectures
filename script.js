bf2e7557236c5397bb365a9b108905398a93a360
Fernando Caique Soares <fndcaique@gmail.com>
2021-03-24 03:16:09 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// Implemente uma função `summationOf(number)` que recebe um número inteiro
+// e retorna o seu somatório.
+
+// Por exemplo:
+
+// - `summationOf(1)` retorna 1: soma de 1 até 1 = 1
+// - `summationOf(3)` retorna 6: soma de 1 até 3 = 1 + 2 + 3 = 6
+// - `summationOf(5)` retorna 15: soma de 1 até 5 = 1 + 2 + 3 + 4 + 5 = 15
