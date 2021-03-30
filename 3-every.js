58182e4c9fa93025063fab12cc10811a307ef290
Fernando Caique Soares <fndcaique@gmail.com>
2021-03-30 17:39:16 -0300

    add every

--- 3-every.js
+++ 3-every.js
@@ -2,3 +2,21 @@ const ages = [23, 32, 17, 16, 34];
 const moreAges = [23, 32, 34];
 
 // todas as pessoas são maiores de idade? // eighteenOrAbove
+
+function eighteenOrAbove(array) {
+  for (let index = 0; index < array.length; index += 1) {
+    if (array[index] < 18) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function eighteenOrAboveWithEvery(array) {
+  return array.every(age => age >= 18);
+}
+
+console.log(eighteenOrAbove(ages));
+console.log(eighteenOrAbove(moreAges));
+console.log(eighteenOrAboveWithEvery(ages));
+console.log(eighteenOrAboveWithEvery(moreAges));
