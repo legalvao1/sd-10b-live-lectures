03edd907ab6f5c0a725e34ede138abd7889c6975
Fernando Caique Soares <fndcaique@gmail.com>
2021-03-30 17:50:56 -0300

    add find

--- 4-find.js
+++ 4-find.js
@@ -7,3 +7,22 @@ const users = [
 ];
 
 // encontrar a primeira pessoa que não é motorista // findANonDriver
+
+// const findANonDriver = (array) => {
+//   return array.find(person => person.isDriver === 'xablau');
+// }
+
+// console.log(users.find(person => person.isDriver === 'xablau'));
+
+const find = (array, callbackFn) => {
+  for (let index = 0; index < array.length; index += 1) {
+    const value = array[index];
+    if (callbackFn(value, index, array) === true) {
+      return value;
+    }
+  }
+  return undefined;
+}
+
+console.log(find(users, (user) => user.isDriver === false));
+
