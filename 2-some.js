7e8202252658a31585838cdc60802635d979c8e3
Fernando Caique Soares <fndcaique@gmail.com>
2021-03-30 16:25:06 -0300

    initial commit

---
+++ 2-some.js
@@ -0,0 +1,4 @@
+const ages = [23, 32, 17, 16, 34];
+const moreAges = [23, 32, 34];
+
+// tem alguma pessoas menor de idade? // lessThan18
---
+++ 3-every.js
@@ -0,0 +1,4 @@
+const ages = [23, 32, 17, 16, 34];
+const moreAges = [23, 32, 34];
+
+// todas as pessoas são maiores de idade? // eighteenOrAbove
