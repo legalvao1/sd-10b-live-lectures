76e5ff86fe7a43ee9dddde84448f2ca2378ee013
Fernando Caique Soares <fndcaique@gmail.com>
2021-03-30 17:25:57 -0300

    add some

--- 2-some.js
+++ 2-some.js
@@ -2,3 +2,21 @@ const ages = [23, 32, 17, 16, 34];
 const moreAges = [23, 32, 34];
 
 // tem alguma pessoas menor de idade? // lessThan18
+
+const lessThan18 = (array) => {
+  for (let index = 0; index < array.length; index += 1) {
+    if (array[index] < 18) {
+      return true;
+    }
+  }
+  return false;
+}
+
+const lessThan18ForEach = (array) => {
+  return array.some(age => age < 18);
+}
+
+console.log(lessThan18(ages));
+console.log(lessThan18(moreAges));
+console.log(lessThan18ForEach(ages));
+console.log(lessThan18ForEach(moreAges));
