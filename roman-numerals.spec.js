5fa68f7aa1b20099cc79aeaf8b8e24869c8d831d
Ícaro Harry <icaro.harry@betrybe.com>
2021-04-19 17:24:53 -0300

    Add package-lock, package.json & roman-numerals test

---
+++ roman-numerals.spec.js
@@ -0,0 +1,50 @@
+const convertToRoman = require('./roman-numerals');
+const assert = require('assert');
+
+describe('Testa a função convertToRoman', () => {
+  test('Converte os números de 1 a 4', () => {
+    expect(convertToRoman(1)).toBe('I');
+    expect(convertToRoman(2)).toBe('II');
+    expect(convertToRoman(3)).toBe('III');
+    expect(convertToRoman(4)).toBe('IV');
+  });
+  
+  test('Converte os números de 5 a 9', () => {
+    expect(convertToRoman(5)).toBe('V');
+    expect(convertToRoman(6)).toBe('VI');
+    expect(convertToRoman(7)).toBe('VII');
+    expect(convertToRoman(8)).toBe('VIII');
+    expect(convertToRoman(9)).toBe('IX');
+  });
+});
+
+// assert.equal(convertToRoman(5), 'V');
+// assert.equal(convertToRoman(6), 'VI');
+// assert.equal(convertToRoman(7), 'VII');
+// assert.equal(convertToRoman(8), 'VIII');
+// assert.equal(convertToRoman(9), 'IX');
+
+// assert.equal(convertToRoman(10), 'X');
+// assert.equal(convertToRoman(11), 'XI');
+// assert.equal(convertToRoman(12), 'XII');
+// assert.equal(convertToRoman(13), 'XIII');
+// assert.equal(convertToRoman(14), 'XIV');
+// assert.equal(convertToRoman(15), 'XV');
+// assert.equal(convertToRoman(26), 'XXVI');
+// assert.equal(convertToRoman(43), 'XLIII');
+
+// assert.equal(convertToRoman(50), 'L');
+// assert.equal(convertToRoman(55), 'LV');
+// assert.equal(convertToRoman(76), 'LXXVI');
+
+// assert.equal(convertToRoman(100), 'C');
+// assert.equal(convertToRoman(200), 'CC');
+// assert.equal(convertToRoman(227), 'CCXXVII');
+
+// assert.equal(convertToRoman(500), 'D');
+// assert.equal(convertToRoman(576), 'DLXXVI');
+// assert.equal(convertToRoman(981), 'CMLXXXI');
+
+// assert.equal(convertToRoman(1000), 'M');
+// assert.equal(convertToRoman(2999), 'MMCMXCIX');
+// assert.equal(convertToRoman(3000), 'MMM');
