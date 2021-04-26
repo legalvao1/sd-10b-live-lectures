a7909c6b0e0327efbf7cdc0841f64f930bf82b4a
Ícaro Harry <icaro.harry@betrybe.com>
2021-04-26 17:45:26 -0300

    Finish lecture 11.1

---
+++ hello-my-friends/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import HelloMyFriends from './components/HelloMyFriends';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <HelloMyFriends />
+      </header>
+    </div>
+  );
+}
+
+export default App;
