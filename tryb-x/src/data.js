80387696cf6182a254d7546b84a367bf3ebb79f0
Ícaro Harry <icaro.harry@betrybe.com>
2021-04-27 15:14:24 -0300

    Init lecture

---
+++ tryb-x/src/data.js
@@ -0,0 +1,49 @@
+export default [
+  {
+    id: 1,
+    name: 'Scott Summers',
+    alias: 'Cyclops',
+    powers: [
+      'Eye Energy Beams',
+    ],
+    img: 'https://cdn.glitch.com/6137de19-12c5-43e0-9704-2252d809dcfb%2FCyclops.png',
+  },
+  {
+    id: 2,
+    name: 'Logan',
+    alias: 'Wolverine',
+    powers: [
+      'Healing Factor',
+      'Animal-Like Senses',
+      'Adamantium Claws and Skeleton',
+    ],
+    img: 'https://cdn.glitch.com/6137de19-12c5-43e0-9704-2252d809dcfb%2Fwolverine.png',
+  },
+  {
+    id: 3,
+    name: 'Charles Xavier',
+    alias: 'Professor X',
+    powers: 'Telepathy',
+    img: 'https://cdn.glitch.com/6137de19-12c5-43e0-9704-2252d809dcfb%2FProfessor_X.png',
+  },
+  {
+    id: 4,
+    name: 'Ororo Munroe',
+    alias: 'Storm',
+    powers: [
+      'Weather Manipulation',
+      'Flight',
+    ],
+    img: 'https://cdn.glitch.com/6137de19-12c5-43e0-9704-2252d809dcfb%2FStorm.png',
+  },
+  {
+    id: 5,
+    name: 'Jean Grey',
+    alias: 'Jean Grey',
+    powers: [
+      'Telekinesis',
+      'Telepathy',
+      'Phoenix Force',
+    ],
+  },
+];
