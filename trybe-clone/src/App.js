b1d37a55276a26b2029b280e608680d33564b414
Ícaro Harry <icaro.harry@betrybe.com>
2021-05-06 16:32:42 -0300

    Init lecture

---
+++ trybe-clone/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './App.css';
+import Content from './components/Content';
+import Header from './components/Header';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Content />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ trybe-clone/src/components/Content.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Content extends React.Component {
+  render() {
+    return (
+      <main className="Content">
+        Content
+      </main>
+    );
+  }
+}
+
+export default Content;
