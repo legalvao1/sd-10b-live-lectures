089dd0ce3444dbdc9b74a81db351f97378685415
Maitê Caetano <maitecaetano@MacBook-Air-de-Maite.local>
2021-05-24 16:00:37 -0300

    removed unused Link import from app.js

--- portolio-exemplo/src/App.js
+++ portolio-exemplo/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route, Link } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Comments from './components/Comments';
 import NotFound from './components/NotFound';
